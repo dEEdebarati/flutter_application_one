d48e0a87ae58a1726926f484725327626a021741
dEE <official.debarati6@gmail.com>
2023-12-22 21:05:28 +0600

    Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Material(
+        child: Center(
+          child: Container(child: Text("The first line on flutter"),
+    ),
+    ),
+    ),
+      
+      );
+      
+  }
+}
